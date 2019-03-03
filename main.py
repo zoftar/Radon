6d67fc0d7ceae0c11d8cba4e3e5313fb7bfa78e7
maartynaa <44441407+maartynaa@users.noreply.github.com>
2019-03-03 17:40:16 +0100

    Read *.bmp

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 from scipy import misc
+import imageio as imageio
 
 
 def main():
@@ -10,11 +11,13 @@ def main():
     l = 10  # rozpiętość układu emiter - detektor
 
     # image = Image.open("square.bmp") #potrzebujemy czegoś, co czyta bmp... poprawnie ;_;
-    image = Image.open("square.png")
+    image = imageio.imread(os.path.join('square.bmp'))
 
     # Plot the original and the radon transformed image
-    plt.subplot(1, 2, 1), plt.imshow(image, cmap='gray')
-    plt.xticks([]), plt.yticks([])
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4.5))
+    ax1.set_title("Original")
+    ax1.imshow(image, cmap=plt.cm.Greys_r)
+    fig.tight_layout()
     plt.show()
 
 
